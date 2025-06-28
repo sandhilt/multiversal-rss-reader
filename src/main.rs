c509a9fff52a98e9ef875e984fb88815e66ab2ab
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-06-28 15:53:43 -0300

    feat: implement RSS feed parser in main.rs
    - Introduce structures for RSS and Entry to represent feed data.
    - Add XML parsing logic to read and display RSS feed elements from an "rss.xml" file.
    - Enhance the main function to handle file reading and XML event processing.
    
    This change significantly improves the project's capability to parse and display RSS feeds, building on the previously added XML dependency.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::BufReader;
+use xml::reader::{EventReader, XmlEvent};
+
+struct Link {
+    rel: Option<String>,
+    url: String,
+}
+
+struct RSS {
+    title: String,
+    subtitle: Option<String>,
+    items: Vec<Entry>,
+    updated: String,
+    link: Vec<Link>,
+    id: String,
+    icon: Option<String>,
+    entry: Vec<Entry>,
+}
+
+struct Entry {
+    title: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let f = File::open("rss.xml")?;
+    let file = BufReader::new(f);
+
+    let parser = EventReader::new(file);
+
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                let local = name.local_name;
+                println!("{:spaces$}+{local}", "", spaces = depth * 2);
+                depth += 1;
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                let local = name.local_name;
+                depth -= 1;
+                println!("{:spaces$}-{local}", "", spaces = depth * 2);
+            }
+            Err(e) => {
+                eprintln!("Error: {e}");
+                break;
+            }
+            // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
+            _ => {}
+        }
+    }
+
+    Ok(())
 }

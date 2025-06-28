555ea7feb611830ee30415898ad2bd5082cde660
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-06-28 14:00:27 -0300

    feat: update flake configuration and add flake.lock
    - Introduce flake.lock to manage dependencies and versions
    - Update flake.nix to include naersk and adjust inputs
    - Refactor outputs to utilize naersk for building the package
    - Enhance development shell with additional Rust tools

--- flake.nix
+++ flake.nix
@@ -1,29 +1,43 @@
 {
   description = "A minimal flake for the multiversal-rss-reader Rust project";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      naersk,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-        # Import project metadata from Cargo.toml
-        manifest = pkgs.lib.importTOML ./Cargo.toml;
-      in {
-        # Package output using buildRustPackage
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = manifest.package.name;
-          version = manifest.package.version;
-          src = pkgs.lib.cleanSource ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-        };
-        # Development shell with common Rust tools
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustc cargo rustfmt clippy rust-analyzer
-          ];
-        };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
       }
     );
 }
